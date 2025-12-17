513046d156cff633812bbc4feee610529155031a
Tomasz Niedzialkowski <tomek_niedzi@o2.pl>
2025-12-17 13:15:34 +0100

    changed default map boundary

--- src/scripts/topological_graph.py
+++ src/scripts/topological_graph.py
@@ -46,9 +46,9 @@ class TopoTree:
         self.latest_cost_benefit = {}
 
         # (x,y) bounds
-        # self.bounds = [(-0.8, 8.7, 0.0), (-2.75, 2.9, 0.0)] # hidden obstacle
+        self.bounds = [(-0.8, 8.7, 0.0), (-2.75, 2.9, 0.0)] # hidden obstacle
         # self.bounds = [(-3.5, 11.25), (-6., 5.8)] # big empty arena
-        self.bounds = [(-0.65, 9.0, 0.0), (-1., 4.5, 0.0)] # custom arena (cluttered)
+        # self.bounds = [(-0.65, 9.0, 0.0), (-1., 4.5, 0.0)] # custom arena (cluttered)
 
         # height=7.0, width=11, center_coorinates=(4.0, 0.0),
 
